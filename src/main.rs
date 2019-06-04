06b53cbcdafc37aa0bc6ef744a4a3bbfb81ac4f0
finkegabriel <finkegabriel@gmail.com>
2019-06-03 18:34:35 -0700

    sorta worksgit add .!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use serde_json::{Result, Value};
+// use serde_json::Result;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
@@ -16,20 +16,18 @@ fn main() -> std::io::Result<()> {
     let mut file = File::open("todo.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    //print!("{}",contents);
+    // print!("{}",contents);
    //-----------------------------------------------------------------------------// 
     let js = contents;
-    //let d: Todo = serde_json::from_str(&js)?;
-    //let mut com = 
-    //println!("{}",d["event"]);
-    
-    
-    /*Command::new("notify-send")
+    let v: Value = serde_json::from_str(&js)?;
+    let data =&v["event"].to_string(); 
+
+    Command::new("notify-send")
                     .arg("-t")
                     .arg("0")
-                    .arg(d["event"])
+                    .arg(data)
                     .output().unwrap_or_else(|e| {
                         panic!("falied to run: {}",e);
-                    });*/
+                    });
     Ok(())
 }
