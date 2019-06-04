287fcfa6989083bf2ab08fbb169dc22646c26d91
finkegabriel <finkegabriel@gmail.com>
2019-06-03 18:51:42 -0700

    getting json to work

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
 use serde_json::{Result, Value};
-// use serde_json::Result;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
 use std::ffi::OsStr;
 
-//#[derive(Serialize, Deserialize)]
-
-struct Todo {
-    event: String,
-    time: String,
-}
-
 fn main() -> std::io::Result<()> {
     let mut file = File::open("todo.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    // print!("{}",contents);
-   //-----------------------------------------------------------------------------// 
+    //-----------------------------------------------------------------------------// 
     let js = contents;
     let v: Value = serde_json::from_str(&js)?;
-    let data =&v["event"].to_string(); 
+    let data =&v["event"][0].to_string(); 
 
     Command::new("notify-send")
                     .arg("-t")
