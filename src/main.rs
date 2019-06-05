82055ceeb1a828412730646116f7785831a0a5fc
finkegabriel <finkegabriel@gmail.com>
2019-06-04 17:58:15 -0700

    worksgit add .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate chrono;
-use chrono::{Datelike, Timelike, Utc};
+extern crate date_time;
 use serde_json::{Result, Value};
 use std::thread::sleep;
 use std::fs::File;
@@ -7,28 +7,41 @@ use std::io::prelude::*;
 use std::process::Command;
 use std::ffi::OsStr;
 use chrono::prelude::*;
+use date_time::time_tuple::{Duration, Time, TimeTuple};
+use std::mem;
+use chrono::Local;
 
-fn main() -> std::io::Result<()> {  
-    let mut file = File::open("todo.txt")?;
+fn main() -> std::io::Result<()> { 
+    // loop {
+    let mut file = File::open("file.json")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     //-----------------------------------------------------------------------------// 
-    let mut foo = "".to_string();
     let js = contents;
-    let v: Value = serde_json::from_str(&js)?;
-    let data = &v["event"][0].to_string();
-    let date = &v["event"][1].to_string();
+    let v: Value = serde_json::from_str(&js).unwrap();
+    let dt = Utc.ymd(2014, 11, 28).and_hms(12, 0, 9);
+    let fixed_dt = dt.with_timezone(&FixedOffset::east(9*3600));
+    let time = &v["event"][1].to_string();
+    let date = Local::now();
+    let systemTime: String = date.format("%H:%M").to_string();
+    let timeRn = &date.format("%H:%M").to_string();
+    let timeEvent = &v["event"][1].as_str().unwrap();
 
-    foo.push_str(&data);
-    foo.push_str(" @ ");
-    foo.push_str(&date);
-    
-    Command::new("notify-send")
+    println!("{}", timeEvent);
+    println!("{}", &date.format("%H:%M").to_string());
+
+    if timeRn == timeEvent {
+        println!("yay!");
+        Command::new("notify-send")
                     .arg("-t")
                     .arg("0")
-                    .arg(foo)
+                    .arg(&v["event"][0].as_str().unwrap())
                     .output().unwrap_or_else(|e| {
                         panic!("falied to run: {}",e);
                     });
+    }else{
+
+    }
     Ok(())
+    
 }
