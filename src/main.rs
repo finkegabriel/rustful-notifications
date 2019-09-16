acb5c5f840ea3ce85aa4327dba9a0b00ac33f2ef
blujay <finkegabriel@gmail.com>
2019-09-15 22:41:40 -0700

    added interpolation over json objects

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ extern crate chrono;
 use chrono::{DateTime, Utc};
 use chrono::prelude::*;
 use std::env;
+use std::collections::HashMap;
+use std::mem;
+
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> =env::args().collect();
@@ -15,19 +18,29 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let v: Value = serde_json::from_str(&contents)?;
-    let m = v["event"].to_string() + " at " + &v["time"].to_string();
-    println!("{}",&v["time"]);
+    // let m = v["event"].to_string() + " at " + &v["time"].to_string();
+    for x in 0..2 {
+        // let time: [String; 50] = [&v[format!("{}",x["event"]]];
+        // println!("{}",&v[format!("{}",x)]);
+        let mut y = &v[format!("{}",x)];
+        let mut event = &y["event"].to_string();
+        println!("{}",event);
+        
+
+
+    }
+    println!("{}",&v["1"]["time"]);
     let now = Local::now();
     let cuTime = now.format("%I");
     
    if cuTime.to_string() == v["time"].to_string() {
-                    Command::new("notify-send")
-                    .arg("-t")
-                    .arg("0")
-                    .arg(m)
-                    .output().unwrap_or_else(|e| {
-                        panic!("falied to run: {}",e);
-                    });
+                    // Command::new("notify-send")
+                    // .arg("-t")
+                    // .arg("0")
+                    // .arg(m)
+                    // .output().unwrap_or_else(|e| {
+                    //     panic!("falied to run: {}",e);
+                    // });
 
         
     }else{
