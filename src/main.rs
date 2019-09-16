69a05ee8639d491e012cfc4f946c68e65bf90972
blujay <finkegabriel@gmail.com>
2019-09-15 22:10:19 -0700

    fixed single function format

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,6 @@ use chrono::{DateTime, Utc};
 use chrono::prelude::*;
 use std::env;
 
-fn handleTime(x: String) {
-    let now = Local::now();
-    let cuTime = now.format("%H");
-    if cuTime.to_string() == x {
-        println!("{} {} {}","good", cuTime, x);
-    }else{
-        println!("{}","not the same");
-     }
-}
-
 fn main() -> std::io::Result<()> {
     let args: Vec<String> =env::args().collect();
     let mut file = File::open(&args[1].to_string())?;
@@ -27,17 +17,22 @@ fn main() -> std::io::Result<()> {
     let v: Value = serde_json::from_str(&contents)?;
     let m = v["event"].to_string() + " at " + &v["time"].to_string();
     println!("{}",&v["time"]);
-    handleTime(v["time"].to_string());
-
-
-
-    Command::new("notify-send")
+    let now = Local::now();
+    let cuTime = now.format("%I");
+    
+   if cuTime.to_string() == v["time"].to_string() {
+                    Command::new("notify-send")
                     .arg("-t")
                     .arg("0")
                     .arg(m)
                     .output().unwrap_or_else(|e| {
                         panic!("falied to run: {}",e);
                     });
+
+        
+    }else{
+        println!("{}","not the same");
+     }
 
         Ok(())
 
