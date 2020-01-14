f719cb4f8a9bdd9c8bd53bda42acb5b3e065c70c
Gabe <finkegabriel@gmail.com>
2020-01-14 00:14:41 -0700

    notifications work

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,29 @@ use std::ffi::OsStr;
 use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use std::process::Command;
 
 #[derive(Serialize, Deserialize)]
 struct Person {
     name: String,
-    age: u8,
-    phones: Vec<String>,
+    event: u8,
+    data: Vec<String>,
 }
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("src/todo.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    // //-----------------------------------------------------------------------------// 
     let js = contents;
-
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
     let p: Person = serde_json::from_str(&js)?;
 
-    // Do things just like with any other Rust data structure.
-    println!("Please call {} at the number {}", p.name, p.phones[0]);
+    Command::new("notify-send")
+    .arg("-t")
+    .arg("0")
+    .arg(&p.data[0])
+    .output().unwrap_or_else(|e| {
+        panic!("falied to run: {}",e);
+    });
 
     Ok(())
 }
