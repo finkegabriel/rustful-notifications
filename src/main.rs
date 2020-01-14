e805b9d6109ace6e88ea01749062ad0fe14d9b5d
Gabe <finkegabriel@gmail.com>
2020-01-14 00:10:25 -0700

    demo works

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,30 @@
+use std::fs::File;
+use std::ffi::OsStr;
+use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
-use std::fs::File;
-use std::io::prelude::*;
-use std::process::Command;
-use std::ffi::OsStr;
 
-//#[derive(Serialize, Deserialize)]
-
-struct Todo {
-    event: String,
-    time: String,
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
 }
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("todo.txt")?;
+    let mut file = File::open("src/todo.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    //print!("{}",contents);
-   //-----------------------------------------------------------------------------// 
+    // //-----------------------------------------------------------------------------// 
     let js = contents;
-    //let d: Todo = serde_json::from_str(&js)?;
-    //let mut com = 
-    //println!("{}",d["event"]);
-    
-    
-    /*Command::new("notify-send")
-                    .arg("-t")
-                    .arg("0")
-                    .arg(d["event"])
-                    .output().unwrap_or_else(|e| {
-                        panic!("falied to run: {}",e);
-                    });*/
+
+    // Parse the string of data into a Person object. This is exactly the
+    // same function as the one that produced serde_json::Value above, but
+    // now we are asking it for a Person as output.
+    let p: Person = serde_json::from_str(&js)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("Please call {} at the number {}", p.name, p.phones[0]);
+
     Ok(())
 }
