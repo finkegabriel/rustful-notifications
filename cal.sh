de1e9a2076bbc4236e9e6fe59dd65543f4f2e9c0
Gabe <finkegabriel@gmail.com>
2020-01-29 10:47:34 -0700

    current

--- cal.sh
+++ cal.sh
@@ -3,18 +3,18 @@ echo Date:
 read date
 echo Event:
 read event 
-echo Time
+echo Time:
 read tim 
 
 echo $event, at, $tim, $date
 
 if [ -f "events/$date.json" ]; then
     echo "$date exist"
-    ADDEVENT="$(jq --arg m "$event" '.event[.event | length] |= . + $m' /home/blujay/code/rustful-notifications/events/$date.json > events/$date.tmp && mv events/$date.tmp events/$date.json )"
-    ADDTIME="$(jq '.time[.time | length] |= . + "'$tim'"' /home/blujay/code/rustful-notifications/events/$date.json > events/$date.tmp && mv events/$date.tmp events/$date.json )"
+    ADDEVENT="$(jq --arg m "$event" '.event[.event | length] |= . + $m' /home/blujay/code/rustful-notifications/events/$date.json > /home/blujay/code/rustful-notifications/events/$date.tmp && mv /home/blujay/code/rustful-notifications/events/$date.tmp /home/blujay/code/rustful-notifications/events/$date.json )"
+    ADDTIME="$(jq '.time[.time | length] |= . + "'$tim'"' /home/blujay/code/rustful-notifications/events/$date.json > events/$date.tmp && mv /home/blujay/code/rustful-notifications/events/$date.tmp /home/blujay/code/rustful-notifications/events/$date.json )"
 else 
     echo "$date does not exist"
-    echo '{ "event": [], "time": [] }' > "events/$date.json"
-    ADDEVENT="$(jq --arg m "$event" '.event[.event | length] |= . + $m' /home/blujay/code/rustful-notifications/events/$date.json > events/$date.tmp && mv events/$date.tmp events/$date.json )"
-    ADDTIME="$(jq '.time[.time | length] |= . + "'$tim'"' /home/blujay/code/rustful-notifications/events/$date.json > events/$date.tmp && mv events/$date.tmp events/$date.json )"
+    echo '{ "event": [], "time": [] }' > "/home/blujay/code/rustful-notifications/events/$date.json"
+    ADDEVENT="$(jq --arg m "$event" '.event[.event | length] |= . + $m' /home/blujay/code/rustful-notifications/events/$date.json > /home/blujay/code/rustful-notifications/events/$date.tmp && mv /home/blujay/code/rustful-notifications/events/$date.tmp /home/blujay/code/rustful-notifications/events/$date.json )"
+    ADDTIME="$(jq '.time[.time | length] |= . + "'$tim'"' /home/blujay/code/rustful-notifications/events/$date.json > /home/blujay/code/rustful-notifications/events/$date.tmp && mv /home/blujay/code/rustful-notifications/events/$date.tmp /home/blujay/code/rustful-notifications/events/$date.json )"
 fi
